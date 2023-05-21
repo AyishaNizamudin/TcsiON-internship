1c80362596e6eabb1f58b240febe3dcf622eca0e
AyishaNizamudin <116575192+AyishaNizamudin@users.noreply.github.com>
2023-05-22 01:03:37 +0530

    Add files via upload

---
+++ SupplierRevenuePage.java
@@ -0,0 +1,45 @@
+package SupplierPage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SupplierRevenuePage {
+	
+	public WebDriver driver;
+	
+	@FindBy(xpath = "//h2[text()='Revenue Breakdown 2023']")
+	private WebElement revenue;
+	
+	@FindBy(xpath = "//div[text()='Total Bookings And Payments']")
+	private WebElement total;
+	
+	public SupplierRevenuePage(WebDriver driver)
+	{
+		
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	public String getTextOfRevenueBreakdown() throws InterruptedException
+	{
+		
+		String text=revenue.getText();
+		Thread.sleep(3000);
+		return text;
+		
+	}
+	
+	public String getTextOfTotalBookings() throws InterruptedException
+	{
+		
+		String text=total.getText();
+		Thread.sleep(3000);
+		return text;
+		
+	}
+	
+	
+}
